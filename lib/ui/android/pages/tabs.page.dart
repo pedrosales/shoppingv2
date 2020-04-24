13791ba9922c1280c821fac73467983af8c3f695
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-24 02:58:26 -0300

    badge no carrinho

--- lib/ui/android/pages/tabs.page.dart
+++ lib/ui/android/pages/tabs.page.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopping/blocs/cart.bloc.dart';
 import 'package:shopping/ui/android/pages/cart.page.dart';
 import 'package:shopping/ui/android/pages/home.page.dart';
 
 class TabsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = Provider.of<CartBloc>(context);
+
     return Scaffold(
       body: TabBarView(
         children: <Widget>[
@@ -21,7 +25,31 @@ class TabsPage extends StatelessWidget {
             icon: new Icon(Icons.home),
           ),
           Tab(
-            icon: new Icon(Icons.shopping_cart),
+            icon: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Icon(Icons.shopping_cart),
+                Container(
+                  width: 18,
+                  height: 18,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(5),
+                    ),
+                  ),
+                  child: Center(
+                    child: Text(
+                      bloc.cart.length.toString(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
           Tab(
             icon: new Icon(
