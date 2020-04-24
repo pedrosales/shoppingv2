02885cf7e0aa14ed2db574ce6c9fe77365259f01
Pedro Ivo de Sales Santos <pedroivo_sales@hotmail.com>
2020-04-24 15:59:26 -0300

    login e logout

--- lib/ui/shared/widgets/account/authenticated-user-card.widget.dart
+++ lib/ui/shared/widgets/account/authenticated-user-card.widget.dart
@@ -1,11 +1,12 @@
+import 'package:shopping/blocs/user.bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shopping/blocs/user.bloc.dart';
 
 class AuthenticatedUserCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var bloc = Provider.of<UserBloc>(context);
+
     return Container(
       child: Center(
         child: Column(
@@ -40,7 +41,7 @@ class AuthenticatedUserCard extends StatelessWidget {
             ),
             FlatButton(
               child: Text("Sair"),
-              onPressed: bloc.logout(),
+              onPressed: bloc.logout,
             ),
           ],
         ),
